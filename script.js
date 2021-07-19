40ede9d7ef994df8928b19399509a9344c584ab4
syv <58623563+syv-dev@users.noreply.github.com>
2021-07-19 15:03:28 +0200

    Creating the pop up window + adding the text

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
+import { msgGenerator } from "./main.js";
+
+const messageOne = msgGenerator();
+const messageTwo = msgGenerator();
+const messageThree = msgGenerator();
+
 const buttonEl = document.getElementById("home-button");
 let homeEl = document.querySelector(".home-container");
+let windowEl = document.querySelector(".message-window");
+let messageOneEl = document.getElementById("text-one");
+let messageTwoEl = document.getElementById("text-two");
+let messageThreeEl = document.getElementById("text-three");
 
 buttonEl.addEventListener("click", () => {
-  console.log("clicked");
   homeEl.classList.add("hidden");
+  windowEl.classList.remove("hidden");
+  messageOneEl.textContent = messageOne;
+  messageTwoEl.textContent = messageTwo;
+  messageThreeEl.textContent = messageThree;
 });
